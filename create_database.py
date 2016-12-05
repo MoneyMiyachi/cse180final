c0cf4a70de1273570ff21c10b592adf1dd7f4926
Adam Klie <aklie@ieng6-248.ucsd.edu>
2016-12-04 22:34:26 -0800

    comments added to Adam's files

--- create_database.py
+++ create_database.py
@@ -1,12 +1,26 @@
+"""File that creates a usable synteny database for our tool to use. 
+   Takes in input from Sibelia run on multiple references and outputs a
+   text file that will be used as input for final_output.py
+   Output is formatted as block number\tstart\tend\tnumber of species that have
+   block"""
+
+
+"""Function that takes in Sibelia text file and reference genome and outputs a
+   only those blocks that are shared between reference and a different species"""
 def get_blocks(synteny_file, reference):
-    blocks = {}
-    reference_id = ''
+    blocks = {} #output variable
+    
+	#will be needed to identify referene from synteny_file
+	reference_id = ''
     reference_number = ''
+
+	#identify reference accession number within reference
     with open(reference, 'r') as f2:
         line = f2.readline()
         parts = line.split()
         reference_id = parts[0][1:]
-        print(reference_id)
+		
+	#identify reference within synteny_file
     with open(synteny_file, 'r') as f:
         for line in f:
             parts = line.split()
@@ -14,8 +28,7 @@ def get_blocks(synteny_file, reference):
                 break
             if parts[2] == reference_id:
                 reference_number = parts[0]
-        print(reference_number)
-        print(line)
+		#parse synteny file to get a list of blocks that are between species
         block_number = 0
         while line != '':
             line = f.readline()
@@ -34,11 +47,15 @@ def get_blocks(synteny_file, reference):
                     wanted_line = f.readline()
                 if(len(sequences) > 1 and reference_number in sequences):
                     blocks[block_number] = len(sequences)
-    #print(blocks)
     return blocks
 
+"""Function that takes in Sibelia synteny block file, reference and blocks that
+   are in reference and other species and returns usable synteny databese for
+   tool"""
 def block_parser(synteny_file, reference, blocks):
-    out = open('syntenydb.txt', 'w')
+    
+	#Get sample_id for reference species
+	out = open('syntenydb.txt', 'w')
     out.write('block number\tstart\tend\tnum blocks\n')
     reference_id = ''
     reference_number = ''
@@ -46,31 +63,31 @@ def block_parser(synteny_file, reference, blocks):
         line = f2.readline()
         parts = line.split()
         reference_id = parts[0][1:]
-        print(reference_id)
-    with open(synteny_file, 'r') as f:
+    
+	#look for lines that include blocks in block parameter
+	with open(synteny_file, 'r') as f:
         for line in f:
             parts = line.split()
             if parts[0][0] == '-':
                 break
             if parts[2] == reference_id:
                 reference_number = parts[0]
-        print(reference_number)
-        print(line)
         while line != '':
             line = f.readline()
             if line == '':
                 break
             parts = line.split()
+
+			#if we find a good block
             if parts[0] == 'Block':
                 block_number = parts[1][1:]
                 line = f.readline()
                 if block_number in blocks:
                     wanted_line = f.readline()
-                    #print(wanted_line)
                     parts = wanted_line.split()
+						
+					#find the synteny regions and output their info
                     while(wanted_line.split()[0][0] != '-'):
-                        #print(wanted_line[0])
-                        #print(wanted_line[0] == reference_number)
                         if(wanted_line[0] == reference_number):
                             parter = wanted_line.split()
                             if int(parter[2]) > int(parter[3]):
@@ -79,8 +96,3 @@ def block_parser(synteny_file, reference, blocks):
                                 out.write("%s\t%s\t%s\t%s\n" %(block_number,parter[3],parter[2], blocks[block_number]))
                         wanted_line = f.readline()
     out.close()
-
-
-#wanted_blocks = get_blocks('blocks_coords.txt', 'reference2.fa.txt')
-#block_parser('blocks_coords.txt', 'reference2.fa.txt', wanted_blocks)
-
--- final_result.py
+++ final_result.py
@@ -1,10 +1,14 @@
+"""File that contains function for combining synteny and snvdesert regions into
+   output file in pipeline."""
+
 import numpy
 import math
 import random
 import operator
 import argparse
 
-
+"""Function that takes in a snvdesert file and a synteny database and outputs a
+   file that combines the information of the two into human readable format"""
 def final_output(SNP_deserts, syntenydb):
     f1 = open(SNP_deserts, 'r')
     f3 = open('./output/output.txt', 'w')
@@ -12,6 +16,7 @@ def final_output(SNP_deserts, syntenydb):
     header = header.rstrip('\n')
     f3.write(header + "\tpercent_synteny\tblocks\tspecificity_score\n")
 
+	#read each snvdesert region from file
     for line1 in f1:
         parts = line1.split()
         #print(parts)
@@ -20,42 +25,41 @@ def final_output(SNP_deserts, syntenydb):
         length = int(parts[2])
         f2 = open(syntenydb, 'r')
         f2.readline()
+		#for each snvdesert region, check to see how much of it if any is in a
+		#synteny block
         for line2 in f2:
             output = []
             parts2 = line2.split()
             start_synteny = int(parts2[1])
             end_synteny = int(parts2[2])
             num_synteny = int(parts2[3])
-            #print(parts2)
             in_synteny_block = False
 
-
+			#if any part is in synteny
             if(start_SNP >= start_synteny and start_SNP <= end_synteny) or (end_SNP >= start_synteny and end_SNP <= end_synteny):
-                if(start_SNP >= start_synteny and end_SNP <= end_synteny):
+                
+				#if whole thing is in synteny
+				if(start_SNP >= start_synteny and end_SNP <= end_synteny):
                     output.append('1.00')
                     output.append(num_synteny)
-                    #print "BLOCK1", (output), line1.rstrip('\n'), start_synteny, end_synteny, length
-
                     f3.write("%s\t%s\t%s\t%s\n" % (line1.rstrip('\n'), output[0], output[1], str(float(output[0])*int(output[1]))))
                     in_synteny_block = True
                     break
 
-
+				#if last part is in synteny
                 elif start_SNP <= start_synteny and end_SNP <= end_synteny:
                     in_synteny = float(end_SNP - start_synteny)/length
                     output.append(str(in_synteny))
                     output.append(num_synteny)
-                    #print "BLOCK 2", (output), line1.rstrip('\n'), start_synteny, end_synteny, length, float(end_SNP-start_synteny)
                     f3.write("%s\t%s\t%s\t%s\n" % (line1.rstrip('\n'), output[0], output[1], str(float(output[0])*int(output[1]))))
                     in_synteny_block = True
                     break
 
-
+				#if first part is in synteny
                 elif(start_SNP >= start_synteny and end_SNP >= end_synteny):
                     in_synteny = float(end_SNP-end_synteny)/length
                     output.append(str(in_synteny))
                     output.append(num_synteny)
-                    #print "BLOCK3", (output), line1.rstrip('\n'), start_synteny, end_synteny, length, float(end_SNP-end_synteny)
                     f3.write("%s\t%s\t%s\t%s\n" % (line1.rstrip('\n'), output[0], output[1], str(float(output[0])*int(output[1]))))
                     in_synteny_block = True
                     break
@@ -69,4 +73,3 @@ def final_output(SNP_deserts, syntenydb):
     f3.close()
 
 
-#output('SNP_desert.txt','syntenydb.txt')
